a8c7c5249b1f6c66d16a342840e9c445dfdcf699
markpedong <mark.pedong01@gmail.com>
2022-03-20 15:42:36 +0800

    Added Test data for the All Products

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -1,7 +1,235 @@
+import "../styles/_allproducts.scss";
+
 const AllProducts = () => {
   return (
-    <div className="container">
-      <p>TEST PRODUCT </p>
+    <div className="container" id="all_products_container">
+      <div
+        className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5"
+        id="product_row"
+      >
+        <div className="card" id="product_container">
+          <img
+            className="card-img-top"
+            src={require("../images/food.jpg")}
+            alt="Card image cap"
+            id="card_products"
+          />
+          <div className="card-body">
+            <p className="card-title">Test Data</p>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <div className="img_link_container">
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="card" id="product_container">
+          <img
+            className="card-img-top"
+            src={require("../images/food.jpg")}
+            alt="Card image cap"
+            id="card_products"
+          />
+          <div className="card-body">
+            <p className="card-title">Test Data</p>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <div className="img_link_container">
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="card" id="product_container">
+          <img
+            className="card-img-top"
+            src={require("../images/food.jpg")}
+            alt="Card image cap"
+            id="card_products"
+          />
+          <div className="card-body">
+            <p className="card-title">Test Data</p>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <div className="img_link_container">
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="card" id="product_container">
+          <img
+            className="card-img-top"
+            src={require("../images/food.jpg")}
+            alt="Card image cap"
+            id="card_products"
+          />
+          <div className="card-body">
+            <p className="card-title">Test Data</p>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <div className="img_link_container">
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="card" id="product_container">
+          <img
+            className="card-img-top"
+            src={require("../images/food.jpg")}
+            alt="Card image cap"
+            id="card_products"
+          />
+          <div className="card-body">
+            <p className="card-title">Test Data</p>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <div className="img_link_container">
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="card" id="product_container">
+          <img
+            className="card-img-top"
+            src={require("../images/food.jpg")}
+            alt="Card image cap"
+            id="card_products"
+          />
+          <div className="card-body">
+            <p className="card-title">Test Data</p>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <div className="img_link_container">
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+              <img
+                src="https://www.freepnglogos.com/uploads/shopee-logo/logo-shopee-png-images-download-shopee-1.png"
+                alt="Card image cap"
+                id="card_img"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
