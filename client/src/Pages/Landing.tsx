eb7dd340b8d4c320cd2a3f67c127eb076c3a57b8
markpedong <mark.pedong01@gmail.com>
2022-05-26 18:07:12 +0800

    Finished for the Landing Page

--- client/src/Pages/Landing.tsx
+++ client/src/Pages/Landing.tsx
@@ -1,5 +1,26 @@
+import { Box, Container, Image, Text } from "@mantine/core";
 import React from "react";
+import LandingPage from "../Images/LandingPage.jpg";
+import { LandingStyles } from "../Styles/Theme/Landing";
 
 export const Landing = () => {
-  return <div>Landing</div>;
+  const { classes } = LandingStyles();
+  return (
+    <Container fluid px="0">
+      <Image
+        src={LandingPage}
+        className={classes.LandingImage}
+        height="100vh"
+      />
+      <Box className={classes.LandingImageText}>
+        <Text className={classes.LandingTitle} align="center" mb="xl">
+          SOLITUDE
+        </Text>
+        <Text weight={600}>
+          Where you can find discounted prices for your favorite online
+          products!
+        </Text>
+      </Box>
+    </Container>
+  );
 };
