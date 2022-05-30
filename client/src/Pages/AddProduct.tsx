548789b39b84b0d4cbaec32dd02eaaaa8557eada
markpedong <mark.pedong01@gmail.com>
2022-05-30 15:58:55 +0800

    Added Style for the Adding Product page

--- client/src/Pages/AddProduct.tsx
+++ client/src/Pages/AddProduct.tsx
@@ -1,36 +1,94 @@
-import { Button, Container, Select, TextInput } from "@mantine/core";
+import {
+  Box,
+  Button,
+  Container,
+  Group,
+  Image,
+  List,
+  Text,
+  TextInput,
+  ThemeIcon,
+  Title,
+} from "@mantine/core";
 import { useNavigate } from "react-router-dom";
 import { AddProductStyles } from "../Styles/Theme/AddProduct";
 import { LoginProps } from "./Login";
+import AddProductDark from "../Images/AddProductDark.svg";
+import AddProductLight from "../Images/AddProductLight.svg";
+import { BsCheckCircleFill } from "react-icons/bs";
+import { LoginContext, useLoginContext } from "../Context/LoginContext";
+import { useState } from "react";
+import { useInputState } from "@mantine/hooks";
 
 export const AddProduct = ({ authenticate }: LoginProps) => {
+  const [focused, setFocused] = useState(false);
+  const [product, setProduct] = useInputState("");
   const navigate = useNavigate();
   const { classes } = AddProductStyles();
-  return (
-    <Container pt="4rem">
-      {/* <Button
-        onClick={() => {
-          authenticate();
-          navigate("/");
-        }}
-      >
-        Logout
-      </Button> */}
-      <div>
-        <TextInput
-          label="Shipping address"
-          placeholder="15329 Huston 21st"
-          classNames={classes}
-        />
+  const { dark } = useLoginContext();
 
-        <Select
-          style={{ marginTop: 20, zIndex: 2 }}
-          data={["React", "Angular", "Svelte", "Vue"]}
-          placeholder="Pick one"
-          label="Your favorite library/framework"
-          classNames={classes}
-        />
-      </div>
-    </Container>
+  return (
+    <div>
+      <Container className={classes.container}>
+        <Box className={classes.Logout}>
+          <Button
+            onClick={() => {
+              authenticate();
+              navigate("/");
+            }}
+          >
+            Logout
+          </Button>
+        </Box>
+        <div className={classes.inner}>
+          <div className={classes.content}>
+            <Title className={classes.title}>
+              Add a new <span className={classes.highlight}>Product</span> to
+              the Website!
+            </Title>
+
+            <List mt={30} spacing="sm" size="sm">
+              <List.Item
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <TextInput
+                  label="Product Name"
+                  placeholder="Input Product Name"
+                  required
+                  classNames={classes}
+                  value={product}
+                  onChange={setProduct}
+                  onFocus={() => setFocused(true)}
+                  onBlur={() => setFocused(false)}
+                  mt="md"
+                  autoComplete="nope"
+                />
+              </List.Item>
+            </List>
+
+            <Group mt={30}>
+              <Button radius="xl" size="md" className={classes.control}>
+                Submit
+              </Button>
+              <Button
+                variant="default"
+                radius="xl"
+                size="md"
+                className={classes.control}
+              >
+                Remove Everything
+              </Button>
+            </Group>
+          </div>
+          <Image
+            src={dark ? AddProductDark : AddProductLight}
+            className={classes.image}
+          />
+        </div>
+      </Container>
+    </div>
   );
 };
--- client/src/Styles/Theme/AddProduct.ts
+++ client/src/Styles/Theme/AddProduct.ts
@@ -1,21 +1,70 @@
 import { createStyles } from "@mantine/core";
 
 export const AddProductStyles = createStyles((theme) => ({
-  root: {
+  container: {
+    paddingTop: theme.spacing.xl * 6,
+  },
+
+  Logout: {
+    marginBottom: theme.spacing.xl * 2,
+    display: "flex",
+    justifyContent: "end",
+  },
+  inner: {
+    display: "flex",
+    justifyContent: "space-between",
+    paddingBottom: theme.spacing.xl * 4,
+  },
+
+  content: {
+    maxWidth: 480,
+    marginRight: theme.spacing.xl * 3,
+
+    [theme.fn.smallerThan("md")]: {
+      maxWidth: "100%",
+      marginRight: 0,
+    },
+  },
+
+  title: {
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: 44,
+    lineHeight: 1.2,
+    fontWeight: 900,
+
+    [theme.fn.smallerThan("xs")]: {
+      fontSize: 28,
+    },
+  },
+
+  control: {
+    [theme.fn.smallerThan("xs")]: {
+      flex: 1,
+    },
+  },
+
+  image: {
+    flex: 1,
+
+    [theme.fn.smallerThan("md")]: {
+      display: "none",
+    },
+  },
+
+  highlight: {
     position: "relative",
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.fn.rgba(theme.colors[theme.primaryColor][6], 0.55)
+        : theme.colors[theme.primaryColor][0],
+    borderRadius: theme.radius.sm,
+    padding: "4px 12px",
   },
 
   input: {
-    height: "auto",
-    paddingTop: 18,
-  },
-
-  label: {
-    position: "absolute",
-    pointerEvents: "none",
-    fontSize: theme.fontSizes.xs,
-    paddingLeft: theme.spacing.sm,
-    paddingTop: theme.spacing.sm / 2,
-    zIndex: 1,
+    "&::placeholder": {
+      transition: "color 150ms ease",
+    },
   },
 }));
