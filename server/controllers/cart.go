95e968d70edb03d84276dacdf539127266b3dec8
markpedong <mark.pedong01@gmail.com>
2023-11-29 21:15:44 +0800

    UPDATE: buyItemFromCart template.

--- server/controllers/cart.go
+++ server/controllers/cart.go
@@ -72,7 +72,36 @@ func RemoveItem(ctx *gin.Context) {
 	})
 }
 
-func CartCheckout(ctx *gin.Context) {}
+func BuyItemFromCart(ctx *gin.Context) {
+	var ids struct {
+		ProductIDs string `json:"product_ids" validate:"required"`
+		UserID     string `json:"user_id" validate:"required"`
+	}
+
+	if err := ctx.ShouldBindJSON(&ids); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid JSON input",
+			"success": false,
+			"status":  http.StatusBadRequest,
+		})
+		return
+	}
+
+	if err := Validate.Struct(ids); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+			"success": false,
+			"status":  http.StatusBadRequest,
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": false,
+		"status":  http.StatusOK,
+		"message": "successfully placed the order!",
+	})
+}
 
 func InstantBuy(ctx *gin.Context) {
 	var ids struct {
