0939a17422784d78e334d9fd6a903aa9600376c4
markpedong <mark.pedong01@gmail.com>
2023-11-29 21:10:57 +0800

    UPDATE: added instant buy template from removed cart function.

--- server/controllers/cart.go
+++ server/controllers/cart.go
@@ -63,7 +63,7 @@ func RemoveItem(ctx *gin.Context) {
 		return
 	}
 
-	// ADD TO CART DATABASE QUERY IS NOT YET IMPLEMENTED
+	// REMOVE FROM CART DATABASE QUERY IS NOT YET IMPLEMENTED
 
 	ctx.JSON(http.StatusOK, gin.H{
 		"success": false,
@@ -74,4 +74,33 @@ func RemoveItem(ctx *gin.Context) {
 
 func CartCheckout(ctx *gin.Context) {}
 
-func InstantBuy(ctx *gin.Context) {}
+func InstantBuy(ctx *gin.Context) {
+	var ids struct {
+		ProductID string `json:"product_id" validate:"required"`
+		UserID    string `json:"user_id" validate:"required"`
+	}
+
+	if err := ctx.ShouldBindJSON(&ids); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid JSON input",
+			"success": false,
+			"status":  http.StatusBadRequest,
+		})
+		return
+	}
+
+	if err := Validate.Struct(ids); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+			"success": false,
+			"status":  http.StatusBadRequest,
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": false,
+		"status":  http.StatusOK,
+		"message": "successfully placed the order!",
+	})
+}
