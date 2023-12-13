2ed002bd4735facd31f4e1cbcf59eceb375c4327
markpedong <mark.pedong01@gmail.com>
2023-12-13 23:22:39 +0800

    added login form container

--- client/src/components/login/loginPage.tsx
+++ client/src/components/login/loginPage.tsx
@@ -1,14 +1,17 @@
 'use client'
 
-import { login } from '@/api'
-import { ProForm, ProFormText } from '@ant-design/pro-components'
-import { FC } from 'react'
-import styles from './styles.module.scss'
-import { LockOutlined, UserOutlined } from '@ant-design/icons'
-import { Button, Checkbox, Typography } from 'antd'
-import { useDispatch } from 'react-redux'
-import { AppDispatch, useAppSelector } from '@/redux/store'
+import { login } from '@/api/index'
+import loginModalCover from '@/public/assets/loginModalCover.png'
+import logo from '@/public/assets/logo.png'
 import { setActiveLoginModal } from '@/redux/features/booleanSlice'
+import { AppDispatch, useAppSelector } from '@/redux/store'
+import { CloseOutlined, LockOutlined, UserOutlined } from '@ant-design/icons'
+import { ProForm, ProFormText } from '@ant-design/pro-components'
+import { Button, Checkbox, Flex, Typography } from 'antd'
+import Image from 'next/image'
+import { FC } from 'react'
+import { useDispatch } from 'react-redux'
+import styles from './styles.module.scss'
 
 const Login: FC = () => {
     const dispatch = useDispatch<AppDispatch>()
@@ -16,39 +19,47 @@ const Login: FC = () => {
 
     return (
         <div className={styles.loginWrapper}>
-            <div className={styles.loginContainer}>
-                <h1>SOLITUDE LOGIN</h1>
-                <p
-                    onClick={() => {
-                        dispatch(setActiveLoginModal(!activeModal))
-                    }}>
-                    close modal
-                </p>
-                <div className={styles.formContainer}>
-                    <ProForm
-                        submitter={false}
-                        onFinish={async params => {
-                            const data = await login({ ...params })
+            <Flex className={styles.loginContainer} justify="space-between" gap={20}>
+                <div className={styles.loginImage}>
+                    <Image alt="loginCover" src={loginModalCover} />
+                </div>
+                <Flex className={styles.loginForm} vertical align="flex-start" justify="space-between">
+                    <CloseOutlined
+                        className={styles.closeIcon}
+                        onClick={() => {
+                            dispatch(setActiveLoginModal(!activeModal))
+                        }}
+                    />
+                    <Flex justify="center">
+                        <h1>SOLITUDE</h1>
+                        {/* <Image src={logo} alt="loginForm" /> */}
+                    </Flex>
+                    <div className={styles.formContainer}>
+                        <ProForm
+                            submitter={false}
+                            onFinish={async params => {
+                                const data = await login({ ...params })
 
-                            console.log('response: ', data.data)
-                        }}>
-                        <ProFormText name="email" placeholder="Email" fieldProps={{ prefix: <UserOutlined /> }} />
-                        <ProFormText.Password
-                            name="password"
-                            placeholder="Password"
-                            fieldProps={{ prefix: <LockOutlined /> }}
-                        />
-                    </ProForm>
-                </div>
-                <div className={styles.passwordContainer}>
-                    <Checkbox>Remember me</Checkbox>
-                    <Typography.Link>Forgot Password</Typography.Link>
-                </div>
-                <Button type="primary">Login</Button>
-                <Typography.Text>
-                    Or <Typography.Link>register now!</Typography.Link>
-                </Typography.Text>
-            </div>
+                                console.log('response: ', data.data)
+                            }}>
+                            <ProFormText name="email" placeholder="Email" fieldProps={{ prefix: <UserOutlined /> }} />
+                            <ProFormText.Password
+                                name="password"
+                                placeholder="Password"
+                                fieldProps={{ prefix: <LockOutlined /> }}
+                            />
+                        </ProForm>
+                    </div>
+                    <Button type="primary">Login</Button>
+                    <Flex>
+                        <Checkbox>Remember me</Checkbox>
+                        <Typography.Link>Forgot Password</Typography.Link>
+                    </Flex>
+                    <Typography.Text>
+                        Or <Typography.Link>register now!</Typography.Link>
+                    </Typography.Text>
+                </Flex>
+            </Flex>
         </div>
     )
 }
--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -2,8 +2,8 @@ import type { ThemeConfig } from 'antd'
 
 const theme: ThemeConfig = {
     token: {
-        fontSize: 16,
-        colorPrimary: '#52c41a',
+        fontSize: 12,
+        colorPrimary: '##000000',
     },
 }
 
