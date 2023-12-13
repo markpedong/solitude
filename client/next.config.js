3fa535c74c0db2cbf2c6b419aca433dfaabbe942
markpedong <mark.pedong01@gmail.com>
2023-12-14 07:01:06 +0800

    removed unused configs.

--- client/next.config.js
+++ client/next.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    content: [
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-
-        // Or if using `src` directory:
-        './src/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
     async rewrites() {
         return [
             {
