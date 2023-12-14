f8b8252d047e0aa35381db81e74bf922cd987c15
markpedong <mark.pedong01@gmail.com>
2023-12-14 22:56:15 +0800

    wrapped around a container.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,25 +1,9 @@
-'use client'
-
-import Login from '@/components/login/loginPage'
-import { setActiveLoginModal } from '@/redux/features/booleanSlice'
-import { AppDispatch, useAppSelector } from '@/redux/store'
-import { Button } from 'antd'
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import LoginContainer from '@/components/login/loginContainer'
 
 const Page = () => {
-    const dispatch = useDispatch<AppDispatch>()
-    const activeModal = useAppSelector(state => state.boolean.activeLoginModal)
     return (
         <div>
-            <Button
-                type="primary"
-                onClick={() => {
-                    dispatch(setActiveLoginModal(true))
-                }}>
-                login
-            </Button>
-            {activeModal && <Login />}
+            <LoginContainer />
         </div>
     )
 }
---
+++ client/src/components/login/loginContainer.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { setActiveLoginModal } from '@/redux/features/booleanSlice'
+import { AppDispatch, useAppSelector } from '@/redux/store'
+import { Button } from 'antd'
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import Login from './loginPage'
+
+const LoginContainer = () => {
+    const dispatch = useDispatch<AppDispatch>()
+    const activeModal = useAppSelector(state => state.boolean.activeLoginModal)
+
+    return (
+        <div>
+            <Button
+                type="primary"
+                onClick={() => {
+                    dispatch(setActiveLoginModal(true))
+                }}>
+                login
+            </Button>
+            {activeModal && <Login />}
+        </div>
+    )
+}
+
+export default LoginContainer
