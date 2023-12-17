d033e241855956f5f7e9b9aa7f2c747e52554825
markpedong <mark.pedong01@gmail.com>
2023-12-17 08:14:58 +0800

    added framer-motion added scrollable for products.

--- client/src/components/landing/components/products/index.tsx
+++ client/src/components/landing/components/products/index.tsx
@@ -1,10 +1,86 @@
 'use client'
 
-import React, { FC } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
 import styles from './styles.module.scss'
+import { motion } from 'framer-motion'
+import { Card } from 'antd'
 
 const Products: FC = () => {
-    return <div className={styles.productWrapper}></div>
+    const [width, setWidth] = useState(0)
+    const carousel = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        setWidth(carousel.current!.scrollWidth - carousel.current!.offsetWidth)
+    }, [])
+
+    return (
+        <div className={styles.productWrapper}>
+            <motion.div className={styles.carousel} ref={carousel}>
+                <motion.div className={styles.innerCarousel} drag="x" dragConstraints={{ right: 0, left: -width }}>
+                    <motion.div className={styles.item}>
+                        <Card
+                            hoverable
+                            style={{ width: 240 }}
+                            cover={
+                                <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+                            }>
+                            <Card.Meta title="Europe Street beat" description="www.instagram.com" />
+                        </Card>
+                    </motion.div>
+                    <motion.div className={styles.item}>
+                        <Card
+                            hoverable
+                            style={{ width: 240 }}
+                            cover={
+                                <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+                            }>
+                            <Card.Meta title="Europe Street beat" description="www.instagram.com" />
+                        </Card>
+                    </motion.div>
+                    <motion.div className={styles.item}>
+                        <Card
+                            hoverable
+                            style={{ width: 240 }}
+                            cover={
+                                <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+                            }>
+                            <Card.Meta title="Europe Street beat" description="www.instagram.com" />
+                        </Card>
+                    </motion.div>
+                    <motion.div className={styles.item}>
+                        <Card
+                            hoverable
+                            style={{ width: 240 }}
+                            cover={
+                                <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+                            }>
+                            <Card.Meta title="Europe Street beat" description="www.instagram.com" />
+                        </Card>
+                    </motion.div>
+                    <motion.div className={styles.item}>
+                        <Card
+                            hoverable
+                            style={{ width: 240 }}
+                            cover={
+                                <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+                            }>
+                            <Card.Meta title="Europe Street beat" description="www.instagram.com" />
+                        </Card>
+                    </motion.div>
+                    <motion.div className={styles.item}>
+                        <Card
+                            hoverable
+                            style={{ width: 240 }}
+                            cover={
+                                <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+                            }>
+                            <Card.Meta title="Europe Street beat" description="www.instagram.com" />
+                        </Card>
+                    </motion.div>
+                </motion.div>
+            </motion.div>
+        </div>
+    )
 }
 
 export default Products
