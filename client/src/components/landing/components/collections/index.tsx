c11fe74ecc55c802feccbbe4f661de1cec91e60f
markpedong <mark.pedong01@gmail.com>
2023-12-18 08:29:48 +0800

    fix the layout

--- client/src/components/landing/components/collections/index.tsx
+++ client/src/components/landing/components/collections/index.tsx
@@ -8,92 +8,90 @@ import Image from 'next/image'
 
 const Collections: FC = () => {
     return (
-        <div className={styles.collectionsContainer}>
-            <Row justify="center">
-                <Col></Col>
-                <Col span={16}>
-                    <Flex justify="space-between">
-                        <div>FEATURED COLLECTIONS</div>
-                        <div>SEE ALL COLLECTIONS</div>
-                    </Flex>
-                    <Space className={styles.featuresItemContainer} size="large">
-                        <div className={styles.featuresItem}>
-                            <Flex className={styles.image} justify="center">
-                                <Image src={test} alt="features" />
-                            </Flex>
-                            <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
-                                <span>COLLECTION #1</span>
-                                <span>Short Description</span>
-                            </Flex>
-                        </div>
-                        <div className={styles.featuresItem}>
-                            <Flex className={styles.image} justify="center">
-                                <Image src={test} alt="features" />
-                            </Flex>
-                            <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
-                                <span>COLLECTION #1</span>
-                                <span>Short Description</span>
-                            </Flex>
-                        </div>
-                        <div className={styles.featuresItem}>
-                            <Flex className={styles.image} justify="center">
-                                <Image src={test} alt="features" />
-                            </Flex>
-                            <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
-                                <span>COLLECTION #1</span>
-                                <span>Short Description</span>
-                            </Flex>
-                        </div>
-                        <div className={styles.featuresItem}>
-                            <Flex className={styles.image} justify="center">
-                                <Image src={test} alt="features" />
-                            </Flex>
-                            <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
-                                <span>COLLECTION #1</span>
-                                <span>Short Description</span>
-                            </Flex>
-                        </div>
-                        <div className={styles.featuresItem}>
-                            <Flex className={styles.image} justify="center">
-                                <Image src={test} alt="features" />
-                            </Flex>
-                            <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
-                                <span>COLLECTION #1</span>
-                                <span>Short Description</span>
-                            </Flex>
-                        </div>
-                        <div className={styles.featuresItem}>
-                            <Flex className={styles.image} justify="center">
-                                <Image src={test} alt="features" />
-                            </Flex>
-                            <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
-                                <span>COLLECTION #1</span>
-                                <span>Short Description</span>
-                            </Flex>
-                        </div>
-                        <div className={styles.featuresItem}>
-                            <Flex className={styles.image} justify="center">
-                                <Image src={test} alt="features" />
-                            </Flex>
-                            <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
-                                <span>COLLECTION #1</span>
-                                <span>Short Description</span>
-                            </Flex>
-                        </div>
-                        <div className={styles.featuresItem}>
-                            <Flex className={styles.image} justify="center">
-                                <Image src={test} alt="features" />
-                            </Flex>
-                            <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
-                                <span>COLLECTION #1</span>
-                                <span>Short Description</span>
-                            </Flex>
-                        </div>
-                    </Space>
-                </Col>
-                <Col></Col>
-            </Row>
-        </div>
+        <Row justify="center" className={styles.collectionsContainer}>
+            <Col></Col>
+            <Col span={16}>
+                <Flex justify="space-between">
+                    <div>FEATURED COLLECTIONS</div>
+                    <div>SEE ALL COLLECTIONS</div>
+                </Flex>
+                <Space className={styles.featuresItemContainer} size="large">
+                    <div className={styles.featuresItem}>
+                        <Flex className={styles.image} justify="center">
+                            <Image src={test} alt="features" />
+                        </Flex>
+                        <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
+                            <span>COLLECTION #1</span>
+                            <span>Short Description</span>
+                        </Flex>
+                    </div>
+                    <div className={styles.featuresItem}>
+                        <Flex className={styles.image} justify="center">
+                            <Image src={test} alt="features" />
+                        </Flex>
+                        <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
+                            <span>COLLECTION #1</span>
+                            <span>Short Description</span>
+                        </Flex>
+                    </div>
+                    <div className={styles.featuresItem}>
+                        <Flex className={styles.image} justify="center">
+                            <Image src={test} alt="features" />
+                        </Flex>
+                        <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
+                            <span>COLLECTION #1</span>
+                            <span>Short Description</span>
+                        </Flex>
+                    </div>
+                    <div className={styles.featuresItem}>
+                        <Flex className={styles.image} justify="center">
+                            <Image src={test} alt="features" />
+                        </Flex>
+                        <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
+                            <span>COLLECTION #1</span>
+                            <span>Short Description</span>
+                        </Flex>
+                    </div>
+                    <div className={styles.featuresItem}>
+                        <Flex className={styles.image} justify="center">
+                            <Image src={test} alt="features" />
+                        </Flex>
+                        <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
+                            <span>COLLECTION #1</span>
+                            <span>Short Description</span>
+                        </Flex>
+                    </div>
+                    <div className={styles.featuresItem}>
+                        <Flex className={styles.image} justify="center">
+                            <Image src={test} alt="features" />
+                        </Flex>
+                        <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
+                            <span>COLLECTION #1</span>
+                            <span>Short Description</span>
+                        </Flex>
+                    </div>
+                    <div className={styles.featuresItem}>
+                        <Flex className={styles.image} justify="center">
+                            <Image src={test} alt="features" />
+                        </Flex>
+                        <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
+                            <span>COLLECTION #1</span>
+                            <span>Short Description</span>
+                        </Flex>
+                    </div>
+                    <div className={styles.featuresItem}>
+                        <Flex className={styles.image} justify="center">
+                            <Image src={test} alt="features" />
+                        </Flex>
+                        <Flex className={styles.featuresTextContainer} justify="center" vertical={true}>
+                            <span>COLLECTION #1</span>
+                            <span>Short Description</span>
+                        </Flex>
+                    </div>
+                </Space>
+            </Col>
+            <Col></Col>
+        </Row>
     )
 }
 
