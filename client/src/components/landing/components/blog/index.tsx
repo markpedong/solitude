b3e058efd0762df5816b039f211a0682fe8119b7
markpedong <mark.pedong01@gmail.com>
2023-12-19 08:27:28 +0800

    finished our blog section

--- client/src/components/landing/components/blog/index.tsx
+++ client/src/components/landing/components/blog/index.tsx
@@ -1,8 +1,10 @@
 'use client'
 
-import { Col, Flex, Row } from 'antd'
+import { Card, Col, Flex, Row, Space } from 'antd'
 import { FC } from 'react'
 import styles from './styles.module.scss'
+
+const { Meta } = Card
 
 const Blog: FC = () => {
     return (
@@ -13,7 +15,49 @@ const Blog: FC = () => {
             </Flex>
             <Row justify="center">
                 <Col></Col>
-                <Col>1</Col>
+                <Col>
+                    <Space>
+                        <Flex className={styles.blogItem} vertical>
+                            <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+                            <Flex className={styles.featuresTextContainer} justify="center" vertical={true} gap={10}>
+                                <span>COLLECTION #1</span>
+                                <span>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo, odio vitae
+                                    turpis egestas fermentum. Sagittis cras morbi orci metus, praesent tortor congue
+                                    aliquam. Lacus ac sed ac semper. Donec eu imperdiet hendrerit ultrices at amet
+                                    purus.
+                                </span>
+                                <span>Read More</span>
+                            </Flex>
+                        </Flex>
+                        <Flex className={styles.blogItem} vertical>
+                            <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+                            <Flex className={styles.featuresTextContainer} justify="center" vertical={true} gap={10}>
+                                <span>COLLECTION #1</span>
+                                <span>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo, odio vitae
+                                    turpis egestas fermentum. Sagittis cras morbi orci metus, praesent tortor congue
+                                    aliquam. Lacus ac sed ac semper. Donec eu imperdiet hendrerit ultrices at amet
+                                    purus.
+                                </span>
+                                <span>Read More</span>
+                            </Flex>
+                        </Flex>
+                        <Flex className={styles.blogItem} vertical>
+                            <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+                            <Flex className={styles.featuresTextContainer} justify="center" vertical={true} gap={10}>
+                                <span>COLLECTION #1</span>
+                                <span>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo, odio vitae
+                                    turpis egestas fermentum. Sagittis cras morbi orci metus, praesent tortor congue
+                                    aliquam. Lacus ac sed ac semper. Donec eu imperdiet hendrerit ultrices at amet
+                                    purus.
+                                </span>
+                                <span>Read More</span>
+                            </Flex>
+                        </Flex>
+                    </Space>
+                </Col>
                 <Col></Col>
             </Row>
         </div>
