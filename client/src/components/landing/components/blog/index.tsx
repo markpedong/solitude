5524b09bc8f0b41d67c672241206227e471b9a3a
markpedong <mark.pedong01@gmail.com>
2023-12-19 08:08:12 +0800

    added title.

--- client/src/components/landing/components/blog/index.tsx
+++ client/src/components/landing/components/blog/index.tsx
@@ -7,8 +7,9 @@ import styles from './styles.module.scss'
 const Blog: FC = () => {
     return (
         <div className={styles.blogContainer}>
-            <Flex className={styles.titleContainer} justify="center">
+            <Flex className={styles.titleContainer} justify="center" align="center" vertical>
                 <span>Our Blog</span>
+                <span>More Articles</span>
             </Flex>
             <Row justify="center">
                 <Col></Col>
