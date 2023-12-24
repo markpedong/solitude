099b5259a978d454c991a7790b4a72f035a02729
markpedong <mark.pedong01@gmail.com>
2023-12-25 00:09:06 +0800

    removed unnecessary code.

---
+++ client/src/app/products/components/products/index.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { PRODUCT_FILTER } from '@/constants'
+import { DownOutlined, SearchOutlined } from '@ant-design/icons'
+import { Col, Flex, Input, Row } from 'antd'
+import { FC } from 'react'
+import { cormorant, jost } from '@/app/page'
+import styles from './styles.module.scss'
+import { TProduct } from '@/api'
+
+const Products: FC<{ data: TProduct[] }> = ({ data }) => {
+    return (
+        <div>
+            <Row justify="center">
+                <Col md={2}></Col>
+                <Col span={24} md={20} className={styles.productWrapper}>
+                    <Flex className={styles.productHeaderContainer} vertical justify="center" align="center" gap={20}>
+                        <span>SOLITUDE / PRODUCTS</span>
+                        <h1 className={cormorant.className}>PRODUCTS</h1>
+                    </Flex>
+                    <Flex className={styles.filterWrapper} justify="space-between" align="center">
+                        <Flex className={styles.filterContainer} justify="space-between">
+                            {PRODUCT_FILTER.map(q => (
+                                <Flex
+                                    className={`${jost.className} ${styles.filterLabel}`}
+                                    justify="center"
+                                    align="center">
+                                    {q.label} <DownOutlined />
+                                </Flex>
+                            ))}
+                        </Flex>
+                        <div>
+                            <Input placeholder="Search" style={{ width: 200 }} suffix={<SearchOutlined />} />
+                        </div>
+                    </Flex>
+                    <div>{data.map(q => q.product_name)}</div>
+                </Col>
+                <Col md={2}></Col>
+            </Row>
+        </div>
+    )
+}
+
+export default Products
