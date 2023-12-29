bb5670203e8cb90218738b9f8502cc978fcdb8eb
markpedong <mark.pedong01@gmail.com>
2023-12-29 22:28:20 +0800

    rendered data.

--- client/src/app/products/[id]/page.tsx
+++ client/src/app/products/[id]/page.tsx
@@ -1,11 +1,29 @@
-import { getProductData } from '@/api'
+import { getProductData, getProducts } from '@/api'
+import Collections from '@/app/components/collections'
 import React, { FC } from 'react'
+import Products from '..'
+import Product from '@/components/products'
 
 type Params = { params: { id: string } }
 
 const ProductItem: FC<Params> = async ({ params }) => {
     const data = await getProductData({ id: params.id })
-    return <div>{JSON.stringify(data)}</div>
+    const products = await getProducts()
+
+    return (
+        <div>
+            {products.data.map(q => (
+                <Product
+                    description={q.description}
+                    id={q.id}
+                    image={q.image}
+                    price={q.price}
+                    product_name={q.product_name}
+                    key={q.id}
+                />
+            ))}
+        </div>
+    )
 }
 
 export default ProductItem
