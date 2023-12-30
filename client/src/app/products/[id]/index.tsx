517b2dc8d84bd9ddce09e34be6bb4e21dc845ed4
markpedong <mark.pedong01@gmail.com>
2023-12-30 22:04:56 +0800

    added styles.

--- client/src/app/products/[id]/index.tsx
+++ client/src/app/products/[id]/index.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import { TProduct } from '@/api'
-import { Col, Flex, Row } from 'antd'
+import { Col, Divider, Flex, Row, Space } from 'antd'
 import React, { FC, memo } from 'react'
 import styles from './styles.module.scss'
 import Product from '@/components/products'
+import { cormorant } from '@/app/page'
 
 type Props = {
     data: TProduct
@@ -16,21 +17,31 @@ const ProductDetails: FC<Props> = ({ data, list }) => {
         <div>
             <Row justify="center">
                 <Col md={2}></Col>
-                <Col span={24} md={20} className={styles.productWrapper}>
+                <Col span={24} md={20}>
                     {JSON.stringify(data)}
-                    <Flex className={styles.productContainer} wrap="wrap" justify="center">
-                        {list.map(q => (
-                            <Product
-                                className={styles.itemContainer}
-                                description={q.description}
-                                id={q.id}
-                                image={q.image}
-                                price={q.price}
-                                product_name={q.product_name}
-                                key={q.id}
-                            />
-                        ))}
-                    </Flex>
+                    <div>
+                        <Flex
+                            className={`${cormorant.className} ${styles.extraProductHeader}`}
+                            justify="center"
+                            vertical>
+                            <Divider />
+                            <span>DAILY DISCOVER</span>
+                            <Divider />
+                        </Flex>
+                        <Flex className={styles.extraProductContainer} wrap="wrap" justify="center">
+                            {list.map(q => (
+                                <Product
+                                    className={styles.itemContainer}
+                                    description={q.description}
+                                    id={q.id}
+                                    image={q.image}
+                                    price={q.price}
+                                    product_name={q.product_name}
+                                    key={q.id}
+                                />
+                            ))}
+                        </Flex>
+                    </div>
                 </Col>
                 <Col md={2}></Col>
             </Row>
