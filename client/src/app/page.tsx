0227f98080c7e2a6475741b3e904d2ce16aab706
markpedong <mark.pedong01@gmail.com>
2024-01-03 08:16:12 +0800

    removed unused component.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,14 @@
 import { TBlog, TCollection, TProduct, getBlogs, getCollections, getProducts } from '@/api'
 import Blog from '@/app/components/blog'
 import Join from '@/app/components/join'
-import Landing from '@/app/components/landing'
 import Collections from '@/app/components/collections'
 import { FC } from 'react'
 import { Cormorant, Jost } from 'next/font/google'
 import Product from '@/components/products'
 import styles from './styles.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
+import landing from '@/public/assets/landing.png'
 
 export const cormorant = Cormorant({ weight: 'variable', subsets: ['latin'] })
 export const jost = Jost({ weight: '400', subsets: ['latin'] })
@@ -18,7 +20,26 @@ const Page: FC = async () => {
 
     return (
         <div>
-            <Landing />
+            <div>
+                <div className={styles.headerContainerText}>
+                    <span className={cormorant.className}>
+                        Find products for your friends, family, and special occasions.
+                    </span>
+                </div>
+                <div className={styles.landingImageContainer}>
+                    <Image src={landing} alt="landing" />
+                </div>
+                <div className={styles.landingButtonContainer}>
+                    <span>
+                        <Link href="/products">SEARCH PRODUCTS</Link>
+                    </span>
+                    <span className={jost.className}>READ OUR CARE GUIDE</span>
+                </div>
+                <div className={styles.featuredContainer}>
+                    <span className={cormorant.className}>Featured Products</span>
+                    <span>Essential products, best values, lower prices</span>
+                </div>
+            </div>
             <div style={{ marginBlockStart: '70dvh' }} />
             <div className={styles.productWrapper}>
                 <div className={styles.carousel}>
