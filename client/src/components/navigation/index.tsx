0c91b9958c14952726c33e81aa6292d2e2d639ba
markpedong <mark.pedong01@gmail.com>
2024-01-12 17:44:52 +0800

    deveopment... removed unused components.

--- client/src/components/navigation/index.tsx
+++ client/src/components/navigation/index.tsx
@@ -1,34 +1,50 @@
 'use client'
 
+import { TProduct, getProducts } from '@/api'
 import { jost } from '@/app/page'
 import { MODAL_FORM_PROPS } from '@/constants'
+import forgotModalCover from '@/public/assets/forgotModalCover.webp'
+import loginModalCover from '@/public/assets/loginModalCover.webp'
 import logo from '@/public/assets/logo.webp'
-import { setActiveLoginModal } from '@/redux/features/booleanSlice'
-import { MenuOutlined, SearchOutlined, UserOutlined } from '@ant-design/icons'
-import { ModalForm } from '@ant-design/pro-components'
-import type { MenuProps } from 'antd'
-import { Col, Drawer, Flex, Grid, Input, Menu, Row, Space } from 'antd'
+import signUpModalCover from '@/public/assets/signUpModalCover.webp'
+import { setActiveLoginForm } from '@/redux/features/booleanSlice'
+import { AppDispatch, useAppSelector } from '@/redux/store'
+import {
+    LockOutlined,
+    MenuOutlined,
+    PhoneOutlined,
+    RightOutlined,
+    SearchOutlined,
+    UserOutlined,
+} from '@ant-design/icons'
+import { ModalForm, ProForm, ProFormText } from '@ant-design/pro-components'
+import { Button, Col, Drawer, Flex, Input, Row, Typography } from 'antd'
 import Image from 'next/image'
 import Link from 'next/link'
-import { FC, useEffect, useMemo, useState } from 'react'
+import { CSSProperties, FC, useEffect, useMemo, useState } from 'react'
 import { useDispatch } from 'react-redux'
-import Login from './components/login'
-import styles from './styles.module.scss'
-import { TProduct, getProducts } from '@/api'
-import { AppDispatch, useAppSelector } from '@/redux/store'
 import Collection from '../collections'
+import styles from './styles.module.scss'
 
 const MenuItem: FC<{ url: string }> = ({ url }) => (
     <a className={styles.menuItem} href={url}>
         {url}
     </a>
 )
+
+const searchStyle: CSSProperties = {
+    position: 'sticky',
+    top: 0,
+}
+
 const Navigation: FC = () => {
     const [open, setOpen] = useState(false)
     const [products, setProducts] = useState<TProduct[]>([])
     const [searchFilter, setSearchFilter] = useState('')
+    const loginForm = useAppSelector(state => state.boolean.activeLoginForm)
+    const create = loginForm === 'create'
+    const forgot = loginForm === 'forgot'
     const dispatch = useDispatch<AppDispatch>()
-    const activeModal = useAppSelector(state => state.boolean.activeLoginModal)
 
     const showDrawer = () => {
         setOpen(true)
@@ -72,11 +88,7 @@ const Navigation: FC = () => {
                 trigger={<SearchOutlined />}
                 submitter={false}
                 style={{ height: 300, overflowY: 'auto' }}>
-                <div
-                    style={{
-                        position: 'sticky',
-                        top: 0,
-                    }}>
+                <div style={searchStyle}>
                     <Input placeholder="eg. Sweater, T-Shirts, Shorts" onChange={handleSearch} />
                 </div>
                 <Flex className={styles.collectionsContainer} justify="center" vertical gap={10}>
@@ -94,9 +106,130 @@ const Navigation: FC = () => {
         )
     }
 
+    const renderLogin = () => (
+        <ModalForm trigger={<UserOutlined />} submitter={false} width={600}>
+            <Flex
+                className={styles.loginContainer}
+                justify="space-between"
+                gap={20}
+                style={{ blockSize: create ? '38rem' : forgot ? '20rem' : '30rem' }}>
+                <div className={styles.loginImage}>
+                    <Image
+                        alt="loginCover"
+                        src={create ? signUpModalCover : forgot ? forgotModalCover : loginModalCover}
+                    />
+                </div>
+                <Flex className={styles.loginForm} vertical>
+                    <Flex className={styles.loginHeader} justify="center" align="center" gap={10}>
+                        <Image src={logo} alt="loginForm" />
+                        <span>SOLITUDE</span>
+                    </Flex>
+                    <Flex className={styles.loginHeaderText} justify="center" vertical>
+                        <h1 className={jost.className}>
+                            {create
+                                ? 'Create an Account'
+                                : forgot
+                                ? 'Forgotten your Password?'
+                                : `Hello, Let's Sign In`}
+                        </h1>
+                        <span>
+                            {create
+                                ? 'Register New Solitude Account'
+                                : forgot
+                                ? 'Enter your email to recover your Account!'
+                                : 'Please register or sign in'}
+                        </span>
+                    </Flex>
+                    <div
+                        className={styles.formContainer}
+                        style={{
+                            margin: `${forgot ? '2.5rem' : '3.5rem'} 0 0.5rem 0`,
+                        }}>
+                        <ProForm
+                            submitter={false}
+                            onFinish={async params => {
+                                // const data = await login({ ...params })
+
+                                console.log('params: ', params)
+                            }}>
+                            {create && (
+                                <Flex gap={10}>
+                                    <ProFormText
+                                        name="first_name"
+                                        placeholder="eg: John"
+                                        label="First Name"
+                                        fieldProps={{ prefix: <UserOutlined />, autoFocus: false }}
+                                    />
+                                    <ProFormText
+                                        name="last_name"
+                                        placeholder="eg: Smith"
+                                        label="Last Name"
+                                        fieldProps={{ autoFocus: false }}
+                                    />
+                                </Flex>
+                            )}
+                            <ProFormText
+                                name="email"
+                                placeholder="your@email.com"
+                                label="Email Address"
+                                fieldProps={{ prefix: <UserOutlined />, autoFocus: false }}
+                            />
+                            {create && (
+                                <ProFormText
+                                    name="phone"
+                                    placeholder="+63 9*********"
+                                    label="Phone Number"
+                                    fieldProps={{ prefix: <PhoneOutlined />, autoFocus: false }}
+                                />
+                            )}
+                            {!forgot && (
+                                <ProFormText.Password
+                                    name="password"
+                                    placeholder="Enter Password"
+                                    label="Password"
+                                    fieldProps={{ prefix: <LockOutlined /> }}
+                                />
+                            )}
+                            {!forgot && (
+                                <Typography.Link
+                                    type="secondary"
+                                    onClick={() => {
+                                        dispatch(setActiveLoginForm('forgot'))
+                                    }}>
+                                    Forgot Password?
+                                </Typography.Link>
+                            )}
+                        </ProForm>
+                    </div>
+                    <Button
+                        className={create ? styles.loginButton : ''}
+                        style={{
+                            marginBlockStart: create ? '2rem' : forgot ? '0.4rem' : '2rem',
+                        }}
+                        type="primary">
+                        {create ? 'SIGN IN' : forgot ? 'RECOVER YOUR ACCOUNT' : 'LOGIN'}
+                    </Button>
+                    <Flex className={styles.createAccountContainer} justify="center">
+                        <Typography.Link
+                            type="secondary"
+                            onClick={() => {
+                                if (loginForm === 'login') {
+                                    dispatch(setActiveLoginForm('create'))
+                                } else {
+                                    dispatch(setActiveLoginForm('login'))
+                                }
+                            }}>
+                            {create ? 'SIGN IN TO ACCOUNT' : 'CREATE AN ACCOUNT'}
+                        </Typography.Link>
+                        <RightOutlined />
+                    </Flex>
+                </Flex>
+            </Flex>
+        </ModalForm>
+    )
+
     return (
         <>
-            {activeModal && <Login />}
             <Row>
                 <Col xs={0} lg={4} />
                 <Col lg={16}>
@@ -112,14 +245,9 @@ const Navigation: FC = () => {
                             <MenuItem url="brands" />
                             <MenuItem url="deals" />
                             <MenuItem url="services" />
-                            <MenuItem url="account" />
                             <Flex className={styles.icons} gap={20}>
                                 {renderSearch()}
-                                <UserOutlined
-                                    onClick={() => {
-                                        dispatch(setActiveLoginModal(true))
-                                    }}
-                                />
+                                {renderLogin()}
                             </Flex>
                         </div>
                         <div className={styles.navigationMobile}>
