fe23510b1cae5b0ea538171fce9ebb82f6da74fd
markpedong <mark.pedong01@gmail.com>
2024-01-13 10:56:08 +0800

    development.

--- client/src/components/footer/index.tsx
+++ client/src/components/footer/index.tsx
@@ -1,52 +1,36 @@
 'use client'
 
-import { Flex, Menu, Space } from 'antd'
-import { MenuProps } from 'antd/lib'
+import { Flex, Space } from 'antd'
+import Link from 'next/link'
 import { FC } from 'react'
 import styles from './styles.module.scss'
-import Link from 'next/link'
 import { jost } from '@/app/page'
 
 const MenuItem: FC<{ url: string }> = ({ url }) => (
-    <div className={jost.className} style={{ letterSpacing: '0.1rem', textTransform: 'uppercase', color: 'black' }}>
-        {url.toUpperCase()}
-    </div>
+    <a className={styles.menuItem} href={url}>
+        {url}
+    </a>
 )
-const items: MenuProps['items'] = [
-    {
-        label: <MenuItem url="products" />,
-        key: 'products',
-    },
-    {
-        label: <MenuItem url="brands" />,
-        key: 'brands',
-    },
-    {
-        label: <MenuItem url="deals" />,
-        key: 'deals',
-    },
-    {
-        label: <MenuItem url="services" />,
-        key: 'services',
-    },
-]
 
 const Footer: FC = () => {
     return (
         <div className={styles.footerWrapper}>
-            <Flex justify="center" align="center" vertical gap={5}>
-                <Menu mode="horizontal" items={items} disabledOverflow />
-                <Space size={30} className={styles.socialMediaContainer}>
-                    <span>
-                        <Link href="/">FACEBOOK</Link>
-                    </span>
-                    <span>
-                        <Link href="/">INSTAGRAM</Link>
-                    </span>
-                    <span>
-                        <Link href="/">SNAPCHAT</Link>
-                    </span>
-                </Space>
+            <Flex className={jost.className} justify="center" align="center" gap={5}>
+                <MenuItem url="products" />
+                <MenuItem url="brands" />
+                <MenuItem url="deals" />
+                <MenuItem url="services" />
+            </Flex>
+            <Flex className={styles.socialMediaContainer} justify="center" align="center">
+                <span>
+                    <Link href="/">FACEBOOK</Link>
+                </span>
+                <span>
+                    <Link href="/">INSTAGRAM</Link>
+                </span>
+                <span>
+                    <Link href="/">SNAPCHAT</Link>
+                </span>
             </Flex>
         </div>
     )
