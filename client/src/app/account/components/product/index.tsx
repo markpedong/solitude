dfdb0513f9f6ed58898c2eec25d761a8c93e2831
atlas <atlas.wbridges@gmail.com>
2024-01-30 16:20:24 +0800

    - fix upload list

--- client/src/app/account/components/product/index.tsx
+++ client/src/app/account/components/product/index.tsx
@@ -2,6 +2,7 @@ import { TProduct } from '@/api'
 import {
     ProForm,
     ProFormDigit,
+    ProFormInstance,
     ProFormSelect,
     ProFormText,
     ProFormTextArea,
@@ -11,7 +12,7 @@ import { Button, Col, Flex, Row } from 'antd'
 import classNames from 'classnames'
 import { Jost } from 'next/font/google'
 import Image from 'next/image'
-import { FC } from 'react'
+import { FC, useRef, useState } from 'react'
 import styles from './styles.module.scss'
 import { motion } from 'framer-motion'
 
@@ -22,33 +23,41 @@ type Props = {
 }
 
 const AddProduct: FC<Props> = ({ products }) => {
+    const [uploadedImages, setUploadedImages] = useState<string[]>([])
+    const formRef = useRef<ProFormInstance>()
+
     return (
-        <>
+        <div className={styles.addProductWrapper}>
             <ProForm
                 grid
                 autoFocusFirstInput={false}
                 submitter={false}
+                formRef={formRef}
                 onFinish={async params => {
                     console.log('params: ', params)
                 }}>
                 <ProForm.Group>
                     <ProFormText colProps={{ span: 12 }}>
                         <Flex className={styles.productFront} vertical align="center">
-                            <Image
-                                src="https://res.cloudinary.com/dyjxuqnn9/image/upload/v1705887851/products/r8cujachll2hhls6iebd.jpg"
-                                alt="product_image"
-                                width={200}
-                                height={200}
-                            />
+                            {uploadedImages?.[0] && (
+                                <Image
+                                    src={uploadedImages?.[0]}
+                                    alt="product_image"
+                                    width={200}
+                                    height={200}
+                                    priority
+                                />
+                            )}
                         </Flex>
-                        <Flex className={styles.galleryContainer} justify="start" wrap="wrap">
-                            {new Array(4)
-                                .fill(
-                                    'https://res.cloudinary.com/dyjxuqnn9/image/upload/v1705887851/products/r8cujachll2hhls6iebd.jpg'
-                                )
-                                ?.map(q => (
-                                    <Image src={q} alt="product_image" width={1000} height={1000} />
-                                ))}
+                        <Flex
+                            className={styles.galleryContainer}
+                            justify="center"
+                            align="center"
+                            wrap="wrap"
+                            data-length={0}>
+                            {uploadedImages?.map((q, i) => (
+                                <Image src={q} alt="product_image" width={1000} height={1000} key={i} priority />
+                            ))}
                             <ProFormUploadButton
                                 name="upload"
                                 fieldProps={{
@@ -67,25 +76,29 @@ const AddProduct: FC<Props> = ({ products }) => {
                             placeholder="Description"
                             colProps={{ span: 24 }}
                         />
-                        <ProFormText label="Price" name="price" placeholder="Price" colProps={{ span: 24 }} />
-                        <ProFormDigit
-                            label="Amount in Stock"
-                            name="stock"
-                            placeholder="in Stock"
-                            colProps={{ span: 24 }}
-                        />
+                        <ProForm.Group>
+                            <ProFormText label="Price" name="price" placeholder="Price" colProps={{ span: 12 }} />
+                            <ProFormDigit
+                                label="Amount in Stock"
+                                name="stock"
+                                placeholder="in Stock"
+                                colProps={{ span: 12 }}
+                            />
+                        </ProForm.Group>
                     </ProFormText>
                 </ProForm.Group>
             </ProForm>
             <Flex justify="center" gap={20}>
                 <motion.div whileTap={{ scale: 0.9 }}>
-                    <Button>Reset</Button>
+                    <Button onClick={() => formRef?.current.resetFields()}>Reset</Button>
                 </motion.div>
                 <motion.div whileTap={{ scale: 0.9 }}>
-                    <Button type="primary">Submit</Button>
+                    <Button type="primary" onClick={() => formRef?.current.submit()}>
+                        Submit
+                    </Button>
                 </motion.div>
             </Flex>
-        </>
+        </div>
     )
 }
 
