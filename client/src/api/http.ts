92d4b5645c45300fc20447e4308617ad0d1a8c15
atlas <atlas.wbridges@gmail.com>
2024-01-31 14:26:19 +0800

    - http update.

--- client/src/api/http.ts
+++ client/src/api/http.ts
@@ -47,22 +47,32 @@ const post = async <T>(url: string, data = {}): Promise<ApiResponse<T>> => {
         body: JSON.stringify(data) || '{}',
     })
     //prettier-ignore
-    const result = await res.json() as ApiResponse<T>
+    const res = await api.json() as ApiResponse<T>
 
-    if (result?.status !== 200) {
-        throttleAlert(result?.message)
-        return result
+    if (res?.status !== 200) {
+        throttleAlert(res?.message)
+        return res
     }
 
-    return result as ApiResponse<T>
+    return res as ApiResponse<T>
 }
 
-const get = async <T>(url: string, data = {}): Promise<ApiResponse<T>> =>
-    await (
-        await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}${url}${stringify(data) ? '?' + stringify(data) : ''}`, {
-            method: 'GET',
-            next: { revalidate: 6000 },
-        })
-    ).json()
+const get = async <T>(url: string, data = {}): Promise<ApiResponse<T>> => {
+    const token = getLocalStorage('token')
+    const api =  await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}${url}${stringify(data) ? '?' + stringify(data) : ''}`, {
+        method: 'GET',
+        headers: {
+            ...(token ? { token: String(token)?.replaceAll(`"`, '') } : {}),
+        },
+    })
+     //prettier-ignore
+     const res = await api.json() as ApiResponse<T>
+
+     if (res?.status !== 200) {
+         return res
+     }
+ 
+     return res as ApiResponse<T>
+}
 
 export { get, post, upload }
