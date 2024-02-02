14fda631b1977fec34216a32598524e39a118b61
markpedong <mark.pedong01@gmail.com>
2024-02-02 23:51:08 +0800

    - models.

--- server/models/models.go
+++ server/models/models.go
@@ -9,10 +9,10 @@ import (
 type User struct {
 	ID             string    `json:"id" gorm:"primaryKey"`
 	CreatedAt      time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt      time.Time `json:"updated_at" gorm:"autoCreateTime"`
-	UserCart       []Product `json:"user_cart" gorm:"foreignKey:ProductID"`
-	AddressDetails []Address `json:"address_details" gorm:"foreignKey:AddressID"`
-	Orders         []Order   `json:"orders" gorm:"foreignKey:OrderID"`
+	UpdatedAt      time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	UserCart       []Product `json:"user_cart" gorm:"foreignKey:UserID"`
+	AddressDetails []Address `json:"address_details" gorm:"foreignKey:UserID"`
+	Orders         []Order   `json:"orders" gorm:"foreignKey:UserID"`
 	FirstName      string    `json:"first_name" validate:"max=10"`
 	LastName       string    `json:"last_name" validate:"max=10"`
 	Password       string    `json:"password" validate:"required,min=6"`
@@ -22,6 +22,40 @@ type User struct {
 	Gender         string    `json:"gender"`
 	Birthday       string    `json:"birthday"`
 }
+
+type Product struct {
+	ProductID   string         `json:"id" gorm:"primaryKey"`
+	UserID      string         `json:"-"`
+	ProductName string         `json:"product_name" validate:"required"`
+	Price       float64        `json:"price" validate:"required"`
+	Rating      int            `json:"rating"`
+	Image       pq.StringArray `json:"image" gorm:"type:text[]"`
+	CreatedAt   int            `json:"created_at" gorm:"autoCreateTime"`
+	Description string         `json:"description" validate:"required"`
+	Stock       int            `json:"stock" validate:"required"`
+	SellerID    string         `json:"seller_id" validate:"required"`
+	BrandID     string         `json:"brand_id" validate:"required"`
+}
+
+type Address struct {
+	AddressID string  `json:"id"  gorm:"primaryKey"`
+	UserID    string  `json:"-"`
+	House     *string `json:"house"`
+	Street    *string `json:"street"`
+	City      *string `json:"city"`
+	Pincode   *string `json:"pin_code"`
+}
+
+type Order struct {
+	OrderID       string    `json:"id" gorm:"primaryKey"`
+	UserID        string    `json:"user_id"`
+	OrderedAt     time.Time `json:"ordered_at" gorm:"autoCreateTime"`
+	Price         int       `json:"price"`
+	Discount      *int      `json:"discount"`
+	PaymentMethod Payment   `json:"payment_method" gorm:"foreignKey:ID"`
+	ProductID     string    `json:"product_id"`
+}
+
 type Seller struct {
 	SellerID   string    `json:"seller_id" gorm:"primaryKey"`
 	CreatedAt  time.Time `json:"created_at" gorm:"autoCreateTime"`
@@ -40,37 +74,6 @@ type Brands struct {
 	BrandID  string    `json:"id" gorm:"primaryKey"`
 	Name     string    `json:"brand_name"`
 	Products []Product `json:"products" gorm:"foreignKey:ProductID"`
-}
-
-type Product struct {
-	ProductID   string         `json:"id" gorm:"primaryKey"`
-	ProductName string         `json:"product_name" validate:"required"`
-	Price       float64        `json:"price" validate:"required"`
-	Rating      int            `json:"rating"`
-	Image       pq.StringArray `json:"image" gorm:"type:text[]"`
-	CreatedAt   int            `json:"created_at" gorm:"autoCreateTime"`
-	Description string         `json:"description" validate:"required"`
-	Stock       int            `json:"stock" validate:"required"`
-	SellerID    string         `json:"seller_id" validate:"required"`
-	BrandID     string         `json:"brand_id" validate:"required"`
-}
-
-type Address struct {
-	AddressID string  `json:"id"  gorm:"primaryKey"`
-	House     *string `json:"house"`
-	Street    *string `json:"street"`
-	City      *string `json:"city"`
-	Pincode   *string `json:"pin_code"`
-}
-
-type Order struct {
-	OrderID string `json:"id" gorm:"primaryKey"`
-	// OrderCart     []Product `json:"order_cart" gorm:"foreignKey:ProductID"`
-	OrderedAt     time.Time `json:"ordered_at" gorm:"autoCreateTime"`
-	Price         int       `json:"price"`
-	Discount      *int      `json:"discount"`
-	PaymentMethod Payment   `json:"payment_method" gorm:"foreignKey:ID"`
-	ProductID     string
 }
 
 type Payment struct {
