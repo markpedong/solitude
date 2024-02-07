78c226cb124a9826df7d805422275ddb014c7126
markpedong <mark.pedong01@gmail.com>
2024-02-07 22:29:44 +0800

    - finished header.

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,7 +1,7 @@
-import { Col, Row } from 'antd'
+import { Col, Input, Row } from 'antd'
 import React from 'react'
 import styles from './styles.module.scss'
-import { DownOutlined } from '@ant-design/icons'
+import { DownOutlined, SearchOutlined, ShopOutlined, ShoppingCartOutlined, UserOutlined } from '@ant-design/icons'
 type Props = {}
 
 const Navbar = (props: Props) => {
@@ -15,6 +15,11 @@ const Navbar = (props: Props) => {
 				<div>on sale</div>
 				<div>new arrivals</div>
 				<div>brands</div>
+			</div>
+			<Input className={styles.input} prefix={<SearchOutlined />} placeholder="Filled" variant="filled" />
+			<div className={styles.userContainer}>
+				<ShoppingCartOutlined />
+				<UserOutlined />
 			</div>
 		</div>
 	)
