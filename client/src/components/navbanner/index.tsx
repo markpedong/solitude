90a640993a2bccb18cffe61bf0489fdc92e34170
markpedong <mark.pedong01@gmail.com>
2024-02-07 21:46:47 +0800

    no message

--- client/src/components/navbanner/index.tsx
+++ client/src/components/navbanner/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import styles from './styles.module.scss'
 import classNames from 'classnames'
+import { CloseOutlined } from '@ant-design/icons'
 
 const NavBanner = () => {
 	return (
 		<div className={classNames(styles.navWrapper)}>
-			<span>Sign up and get 20% off to your first order. Sign Up Now</span>
+			<span>
+				Sign up and get 20% off to your first order. <p>Sign Up Now</p>
+			</span>
+			<CloseOutlined />
 		</div>
 	)
 }
