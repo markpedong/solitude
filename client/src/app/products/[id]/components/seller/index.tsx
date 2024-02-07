839531e013732e0ff958c9ec07831b645c5a4a98
atlas <atlas.wbridges@gmail.com>
2024-02-07 16:24:35 +0800

    no message

--- client/src/app/products/[id]/components/seller/index.tsx
+++ client/src/app/products/[id]/components/seller/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss'
 import { Button, Col, Divider, Flex, Row } from 'antd'
 import { SellerData } from '@/api'
 import Image from 'next/image'
-import { ShopOutlined, WechatOutlined } from '@ant-design/icons'
+import { ShopOutlined } from '@ant-design/icons'
 
 type Props = {
     data: SellerData
@@ -18,8 +18,8 @@ const Seller: FC<Props> = memo(({ data }) => {
                     <div className={styles.sellerDescContainer}>
                         <Image src={data.avatar} width={200} height={200} alt={data.seller_name} />
                         <Flex className={styles.sellerDesc} vertical>
-                            <span>{data.seller_name}</span>
-                            <span>Active 1 Minute Ago</span>
+                            <span className={styles.title}>{data.seller_name}</span>
+                            {/* <span>Active 1 Minute Ago</span> */}
                             <div>
                                 {/* <Button icon={<WechatOutlined />}>Chat Now</Button> */}
                                 <Button type='primary' icon={<ShopOutlined />}>View Shop</Button>
