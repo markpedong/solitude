4d5d3824919c979c5ecaa2bfb08abedbe861eb2c
atlas <atlas.wbridges@gmail.com>
2024-02-08 19:05:10 +0800

    - started development for product page.

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import styles from './styles.module.scss'
+import { RightOutlined } from '@ant-design/icons'
 
 type Props = {}
 
 const Products = (props: Props) => {
-  return (
-    <div>
-      
-    </div>
-  )
+	return (
+		<div className={styles.productWrapper}>
+			<div className={styles.productCategory}>
+				<span>Home</span>
+				<RightOutlined />
+				<span>Shop</span>
+				<RightOutlined />
+				<span>T-shirts</span>
+			</div>
+      <div>1</div>
+		</div>
+	)
 }
 
 export default Products
