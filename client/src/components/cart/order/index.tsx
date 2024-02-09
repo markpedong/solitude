5d975ae9bc287068f882d0918cac88def7f837a2
markpedong <mark.pedong01@gmail.com>
2024-02-09 19:45:41 +0800

    no message

---
+++ client/src/components/cart/order/index.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from 'react'
+import styles from './styles.module.scss'
+import { Divider } from 'antd'
+import Image from 'next/image'
+import { DeleteOutlined, MinusOutlined, PlusOutlined } from '@ant-design/icons'
+
+type Props = {
+	divider?: boolean
+}
+
+const Order: FC<Props> = ({ divider = true }) => {
+	return (
+		<>
+			<div className={styles.orderWrapper}>
+				<Image className={styles.image} src={'/assets/pen.png'} alt="image" width={100} height={100} />
+				<div className={styles.textContainer}>
+					<div className={styles.title}>
+						<span>Gradient Graphic T-shirt</span>
+						<DeleteOutlined />
+					</div>
+					<div className={styles.variant}>
+						<span>
+							Size: <p>Large</p>
+						</span>
+						<span>
+							Color: <p>White</p>
+						</span>
+					</div>
+					<div className={styles.price}>
+						<span>$145</span>
+						<div className={styles.addItemContainer}>
+							<MinusOutlined />
+							<span>1</span>
+							<PlusOutlined />
+						</div>
+					</div>
+				</div>
+			</div>
+			{divider && <Divider />}
+		</>
+	)
+}
+
+export default Order
--- client/src/components/landing/navbar/index.tsx
+++ client/src/components/landing/navbar/index.tsx
@@ -25,33 +25,35 @@ const Navbar = () => {
 	}
 
 	return (
-		<div className={styles.navbarWrapper}>
-			<div className={styles.navbarContainer}>
-				<MenuOutlined className={styles.navbarMobile} onClick={showDrawer} />
-				<Drawer onClose={onClose} open={open}>
-					<MenuItem title="shop" />
-					<MenuItem title="on sale" />
-					<MenuItem title="new arrivals" />
-					<MenuItem title="brands" />
-				</Drawer>
-				<div className={styles.navbarLogo}>SOLITUDE</div>
-				<div className={styles.navbarHeader}>
-					<div>
-						shop <DownOutlined />
+		<>
+			<div className={styles.navbarWrapper}>
+				<div className={styles.navbarContainer}>
+					<MenuOutlined className={styles.navbarMobile} onClick={showDrawer} />
+					<Drawer onClose={onClose} open={open}>
+						<MenuItem title="shop" />
+						<MenuItem title="on sale" />
+						<MenuItem title="new arrivals" />
+						<MenuItem title="brands" />
+					</Drawer>
+					<div className={styles.navbarLogo}>SOLITUDE</div>
+					<div className={styles.navbarHeader}>
+						<div>
+							shop <DownOutlined />
+						</div>
+						<div>on sale</div>
+						<div>new arrivals</div>
+						<div>brands</div>
 					</div>
-					<div>on sale</div>
-					<div>new arrivals</div>
-					<div>brands</div>
-				</div>
-				<Input className={styles.input} prefix={<SearchOutlined />} placeholder="Filled" variant="filled" />
-				<div className={styles.userContainer}>
-					<SearchOutlined className={styles.smallInput} />
-					<ShoppingCartOutlined />
-					<UserOutlined />
+					<Input className={styles.input} prefix={<SearchOutlined />} placeholder="Filled" variant="filled" />
+					<div className={styles.userContainer}>
+						<SearchOutlined className={styles.smallInput} />
+						<ShoppingCartOutlined />
+						<UserOutlined />
+					</div>
 				</div>
 			</div>
 			<Divider />
-		</div>
+		</>
 	)
 }
 
