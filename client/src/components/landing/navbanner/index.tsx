0a052e015336aeec676d0e9fab95dae947956f84
atlas <atlas.wbridges@gmail.com>
2024-03-26 17:03:51 +0800

    fix navbanner.

--- client/src/components/landing/navbanner/index.tsx
+++ client/src/components/landing/navbanner/index.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 import styles from './styles.module.scss'
 import { CloseOutlined } from '@ant-design/icons'
 import { AppDispatch, useAppSelector } from '@/redux/store'
-import { setIsBannerHidden } from '@/redux/features/booleanSlice'
+import { setIsBannerHidden, setLoginModalOpen } from '@/redux/features/booleanSlice'
 import { useDispatch } from 'react-redux'
 
 const NavBanner = () => {
@@ -14,7 +14,15 @@ const NavBanner = () => {
 	return (
 		<div className={styles.navWrapper} style={{ display: bannerHidden ? 'none' : '' }}>
 			<span>
-				Sign up and get 20% off to your first order. <p>Sign Up Now</p>
+				Sign up and get 20% off to your first order.{' '}
+				<p
+					className='cursor-pointer'
+					onClick={() => {
+						dispatch(setLoginModalOpen(true))
+					}}
+				>
+					Sign Up Now
+				</p>
 			</span>
 			<CloseOutlined onClick={() => dispatch(setIsBannerHidden(true))} />
 		</div>
